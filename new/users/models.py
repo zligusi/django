de54e583e71b04f85651db0abb8ce0cb94b08f3e
zligusi <tort24abcd@gmaill.com>
2026-07-14 22:04:01 +0200

    work to models.py

--- new/users/models.py
+++ new/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=20)
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=20)
+    data = models.DateField('birthdate') 
+
+def __str__(self):
+    return self.username
