de884ad53262b71d2a39d7c37ddbe3d90836883f
zligusi <tort24abcd@gmaill.com>
2026-07-14 22:15:48 +0200

    edit regirtration email

--- new/users/models.py
+++ new/users/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 class User(models.Model):
     username = models.CharField(max_length=20)
-    email = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
     password = models.CharField(max_length=20)
-    data = models.DateField('birthdate') 
+    data = models.DateField('birthdate', null=True, blank=True)
 
 def __str__(self):
     return self.username
