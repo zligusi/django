b1f1394aacb8057a5ae601b6f9856b5c51dec2f1
zligusi <tort24abcd@gmaill.com>
2026-07-15 22:18:03 +0200

    500 no url create

--- new/users/views.py
+++ new/users/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from .models import User
 
 def  reg(request):                                                                                                      
-    return render(request , 'users/reg.html')
+    return render(request , 'users/data_registr.html')
+
+def create(request):
+    return render(request , 'users/create.html')
